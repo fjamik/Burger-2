0f0abe84a6b8384d8fad6cd87bacbe6958ee54c3
fjamik <fjamik@gmail.com>
2019-05-10 10:01:29 -0400

    new update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
+// Include the dotenv npm package
+require('dotenv').config();
+module.exports =
 {
+
   "development": {
     "username": "root",
-    "password": "qwerty555",
+    "password": process.env.DB_PWD,
     "database": "burgers_db",
     "host": "127.0.0.1",
     "port": 3306,
